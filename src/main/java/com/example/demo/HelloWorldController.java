d894ac2086e3297241a75fcf7934f84feac862f1
nitkis <nitin.123048@gmail.com>
2020-08-16 22:10:16 +0530

    first commit

---
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
+
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+    @ResponseBody
+    public HelloWorldResponse get(@RequestBody HelloWordRequest request) {
+        if (request.getName() == null) {
+            return new HelloWorldResponse("invalid request");
+        }
+
+        if (request.getName().equals("nitin")) {
+            return new HelloWorldResponse("hi nitin");
+        } else {
+            return new HelloWorldResponse("hi neha");
+        }
+    }
+}
